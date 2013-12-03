624fae958aa1cded0cd2e629250e6219ec16eada
Jens Goeman <jens.goeman@ida-mediafoundry.be>
2013-12-03 17:49:13 +0100

    Added other label + type reference
    Signed-off-by: Jens Goeman <jens.goeman@ida-mediafoundry.be>

--- demo-guidelines/demo-guidelines/View/Custom View/HomeView.m
+++ demo-guidelines/demo-guidelines/View/Custom View/HomeView.m
@@ -10,8 +10,11 @@
 #import "UIColor+DemoGuidelinesTheme.h"
 #import "UIFont+DemoGuidelinesTheme.h"
 
+#define MARGIN_BETWEEN_VIEWS 0.0
+
 @interface HomeView () {
-    UILabel *_companyLabel;
+    UILabel *_titleLabel;
+    UILabel *_subLabel;
 }
 
 @end
@@ -30,31 +33,49 @@
 
 - (void)_setupHomeView {
     self.backgroundColor = [UIColor whiteColor];
-    [self _configureCompanyLabel];
+    [self _configureTitleLabel];
+    [self _configureSubLabel];
 }
+
 
 #pragma mark - View life cycle
 
 - (void)layoutSubviews {
     [super layoutSubviews];
-    _companyLabel.frame = [self _rectForCompanyLabel];
+    _titleLabel.frame = [self _rectForCompanyLabel];
+    _subLabel.frame = [self _rectForSubLabel];
 }
 
 
 #pragma mark - View configuration
-- (void)_configureCompanyLabel {
-    _companyLabel = [[UILabel alloc] init];
-    _companyLabel.backgroundColor = [UIColor clearColor];
-    _companyLabel.textColor = [UIColor idaRedColor];
-    _companyLabel.font = [UIFont istokBoldWithSize:26.0];
-    _companyLabel.textAlignment = NSTextAlignmentCenter;
-    _companyLabel.frame = [self _rectForCompanyLabel];
-    _companyLabel.text = @"iDA-MediaFoundry";
-    [self addSubview:_companyLabel];
+- (void)_configureTitleLabel {
+    _titleLabel = [[UILabel alloc] init];
+    _titleLabel.backgroundColor = [UIColor clearColor];
+    _titleLabel.textColor = [UIColor idaRedColor];
+    _titleLabel.font = [UIFont istokBoldWithSize:26.0];
+    _titleLabel.textAlignment = NSTextAlignmentCenter;
+    _titleLabel.frame = [self _rectForCompanyLabel];
+    _titleLabel.text = @"iDA-MediaFoundry";
+    [self addSubview:_titleLabel];
 }
 
 - (CGRect)_rectForCompanyLabel {
-    return CGRectMake(0.0, self.bounds.size.height/4, self.bounds.size.width, _companyLabel.font.lineHeight);
+    return CGRectMake(0.0, self.bounds.size.height/4, self.bounds.size.width, _titleLabel.font.lineHeight);
+}
+
+- (void)_configureSubLabel {
+    _subLabel = [[UILabel alloc] init];
+    _subLabel.backgroundColor = [UIColor clearColor];
+    _subLabel.textColor = [UIColor idaDarkGrayColor];
+    _subLabel.font = [UIFont istokRegularWithSize:14.0];
+    _subLabel.textAlignment = NSTextAlignmentCenter;
+    _subLabel.frame = [self _rectForSubLabel];
+    _subLabel.text = @"iOS Coding Guidelines";
+    [self addSubview:_subLabel];
+}
+
+- (CGRect)_rectForSubLabel {
+    return CGRectMake(0.0, _titleLabel.frame.origin.y + _titleLabel.frame.size.height + MARGIN_BETWEEN_VIEWS , self.bounds.size.width, _titleLabel.font.lineHeight);
 }
 
 @end
